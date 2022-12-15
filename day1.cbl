3e8b7339c5ca0a70537a2949cdd6cd49309eee86
Kevin Jones <kevin@vcsjones.com>
2022-12-14 20:02:46 -0500

    Part 2 of Day 1 in COBOL

--- day1.cbl
+++ day1.cbl
@@ -18,20 +18,34 @@ WORKING-STORAGE SECTION.
     02 WS-CALORIES PIC 9(9).
 77 WS-EOF PIC A(1).
 77 WS-SUM PIC 9(9).
-77 WS-BIGGEST PIC 9(9).
+01 WS-TOP.
+   05 WS-BIGGEST PIC 9(9) OCCURS 3 TIMES.
 
 
 PROCEDURE DIVISION.
-    SET WS-SUM TO 0
+    MOVE ZEROES TO WS-TOP
+    MOVE 0 TO WS-SUM
     OPEN INPUT DAY1DATA.
         PERFORM UNTIL WS-EOF='Y'
             READ DAY1DATA INTO WS-ELF
                 AT END MOVE 'Y' TO WS-EOF
                 NOT AT END
                     IF (WS-CALORIES EQUAL TO SPACE)
-                        IF (WS-SUM > WS-BIGGEST)
-                            MOVE WS-SUM TO WS-BIGGEST
+                        IF (WS-SUM > WS-BIGGEST(1))
+                            MOVE WS-BIGGEST(2) TO WS-BIGGEST(3)
+                            MOVE WS-BIGGEST(1) TO WS-BIGGEST(2)
+                            MOVE WS-SUM TO WS-BIGGEST(1)
+                        ELSE
+                            IF (WS-SUM > WS-BIGGEST(2))
+                                MOVE WS-BIGGEST(2) TO WS-BIGGEST(3)
+                                MOVE WS-SUM TO WS-BIGGEST(2)
+                            ELSE
+                                IF (WS-SUM > WS-BIGGEST(3))
+                                    MOVE WS-SUM TO WS-BIGGEST(3)
+                                END-IF
+                            END-IF
                         END-IF
+
                         SET WS-SUM TO 0
                     ELSE
                         COMPUTE WS-SUM = WS-SUM + FUNCTION NUMVAL(WS-CALORIES)
@@ -39,11 +53,23 @@ PROCEDURE DIVISION.
             END-READ
         END-PERFORM.
 
-        IF (WS-SUM > WS-BIGGEST)
-            MOVE WS-SUM TO WS-BIGGEST
+        IF (WS-SUM > WS-BIGGEST(1))
+            MOVE WS-BIGGEST(2) TO WS-BIGGEST(3)
+            MOVE WS-BIGGEST(1) TO WS-BIGGEST(2)
+            MOVE WS-SUM TO WS-BIGGEST(1)
+        ELSE
+            IF (WS-SUM > WS-BIGGEST(2))
+                MOVE WS-BIGGEST(2) TO WS-BIGGEST(3)
+                MOVE WS-SUM TO WS-BIGGEST(2)
+            ELSE
+                IF (WS-SUM > WS-BIGGEST(3))
+                    MOVE WS-SUM TO WS-BIGGEST(3)
+                END-IF
+            END-IF
         END-IF
 
-        DISPLAY WS-BIGGEST
+        DISPLAY 'PART 1: ' WS-BIGGEST(1)
+        DISPLAY 'PART 2: ' FUNCTION SUM (WS-BIGGEST(1) WS-BIGGEST(2) WS-BIGGEST(3))
     CLOSE DAY1DATA.
     STOP RUN.
 
